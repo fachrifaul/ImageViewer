9da54b80daee5d1b463f0b4e72709efce0c81258
Fachri Febrian <fachripaul@gmail.com>
2019-08-11 20:42:18 +0700

    first commit

---
+++ ImageViewer/AppDelegate.swift
@@ -0,0 +1,53 @@
+//
+//  AppDelegate.swift
+//  ImageViewer
+//
+//  Created by Fachri Febrian on 11/08/2019.
+//  Copyright © 2019 fachrifaul. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    static let IsLightStatusBar = false
+    
+    var window: UIWindow?
+    
+    public func application(_: UIApplication, willFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let localController = PhotosController(dataSourceType: .local)
+        localController.title = "Local"
+        let localNavigationController = UINavigationController(rootViewController: localController)
+        
+        let remoteController = PhotosController(dataSourceType: .remote)
+        remoteController.title = "Remote"
+        let remoteNavigationController = UINavigationController(rootViewController: remoteController)
+        
+        let photoController = PhotoController()
+        photoController.title = "Plain VC"
+        let photoNavigationController = UINavigationController(rootViewController: photoController)
+        
+        if AppDelegate.IsLightStatusBar {
+            UINavigationBar.appearance().barTintColor = .orange
+            UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+            remoteNavigationController.navigationBar.barStyle = .black
+            localNavigationController.navigationBar.barStyle = .black
+        }
+        
+        let tabBarController = UITabBarController()
+        tabBarController.setViewControllers(
+            [localNavigationController, remoteNavigationController, photoNavigationController],
+            animated: false
+        )
+        
+        self.window!.rootViewController = tabBarController
+        self.window!.makeKeyAndVisible()
+        
+        return true
+    }
+
+}
+
---
+++ ImageViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageViewer
+//
+//  Created by Fachri Febrian on 11/08/2019.
+//  Copyright © 2019 fachrifaul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
